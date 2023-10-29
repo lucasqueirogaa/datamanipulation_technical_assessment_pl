cb45a5298a688119ef721e0b67eb00651dc7e196
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-10-29 17:57:54 -0300

    feat: Create read xls and save on DB clients method

--- src/controllers/readerController.ts
+++ src/controllers/readerController.ts
@@ -10,7 +10,8 @@ import { IBoxesSheets } from "../types/Boxes";
 import { ISplittersSheets } from "../types/Splitters";
 
 import logger from "../log/logger";
-import { IUsers, IUsersSheets } from "../types/User";
+import { IClientsSheets } from "../types/Clients";
+import ClientsModel from "../models/clientsModel";
 
 const filePath = path.resolve(__dirname, "../../files/data.xls");
 
@@ -142,6 +143,57 @@ const readXlsController = {
       res
         .status(200)
         .json({ message: "Sucess with return", Splitters: response });
+    } catch (error) {
+      logger.error(error);
+      res.status(500).json({ message: "Something get wrong" });
+    }
+  },
+  clients: async (res: Response) => {
+    try {
+      const file = xlsx.readFile(filePath);
+      const sheet = file.Sheets["Clients"];
+      const data: IClientsSheets[] = xlsx.utils.sheet_to_json(sheet);
+
+      if (data.length < 1) {
+        return res.status(400).json({ message: "Error with file" });
+      }
+
+      const options = {
+        method: "GET",
+        url: `https://randomuser.me/api/?results=${data.length}&nat=BR`,
+      };
+
+      const randomUsers = await axios.request(options);
+
+      if (!randomUsers.data) {
+        return res.status(400).json({ message: "Error with random users API" });
+      }
+
+      const userArray = data.map((obj, index) => {
+        const currentRandomUser = randomUsers.data.results[index];
+
+        return {
+          address: `${currentRandomUser.location.street.name} ${currentRandomUser.location.street.number} ${currentRandomUser.location.postcode} ${currentRandomUser.location.city} ${currentRandomUser.location.state} ${currentRandomUser.location.country}`,
+          project: process.env.PROJECT,
+          box: obj.Box || "",
+          lat: obj.Latitude || 0,
+          lng: obj.Longitude || 0,
+          force: true,
+          auto_connect: true,
+          username: currentRandomUser.login.username,
+          email: currentRandomUser.email,
+          name: `${currentRandomUser.name.first} ${currentRandomUser.name.last}`,
+          code: `${(
+            currentRandomUser.name.first +
+            "." +
+            currentRandomUser.name.last
+          ).toLocaleLowerCase()}`,
+        };
+      });
+
+      const response = await ClientsModel.insertMany(userArray);
+
+      res.status(200).json({ message: "Sucess with return", Users: response });
     } catch (error) {
       logger.error(error);
       res.status(500).json({ message: "Something get wrong" });
