1c7b60860d2db375526f2e304b61f4bd49440cda
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-10-29 17:57:00 -0300

    feat: Create clients types

---
+++ src/types/Clients.ts
@@ -0,0 +1,20 @@
+export interface IClientsSheets {
+  Latitude: String;
+  Longitude: String;
+  Box: String;
+  Status: String;
+}
+
+export interface IClients {
+  address: String;
+  project: String;
+  box: String;
+  lat: String;
+  lng: String;
+  force: Boolean;
+  auto_connect: Boolean;
+  username: String;
+  email: String;
+  name: String;
+  code: String;
+}
