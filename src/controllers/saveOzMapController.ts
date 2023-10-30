10ef74d02205c475e122ac1fad8aeecd2402adcf
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-10-30 03:59:08 -0300

    refactor: Optmize save boxes function

--- src/controllers/saveOzMapController.ts
+++ src/controllers/saveOzMapController.ts
@@ -9,64 +9,53 @@ import logger from "../log/logger";
 const saveOzMapController = {
   boxes: async (res: Response) => {
     const boxes: IBoxes[] = await BoxesModel.find({});
-    const response = [];
+    const axiosInstance = axios.create({
+      baseURL: "https://data-manipulation-6.ozmap.com.br:9994/api/v2/",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: process.env.AUTHORIZATION,
+      },
+    });
 
     if (boxes.length < 1) {
       return res.status(400).json({ message: "You need to have boxes on DB" });
     }
 
-    const axiosRequests = boxes.map(async (box) => {
+    const sortedArray = boxes.sort((a, b) => {
+      if (a.name === "" && b.name !== "") {
+        return 1;
+      } else if (a.name !== "" && b.name === "") {
+        return -1;
+      }
+      return 0;
+    });
+
+    const axiosRequests = sortedArray.map(async (box) => {
       try {
-        const res = await axios.post(
-          "https://data-manipulation-6.ozmap.com.br:9994/api/v2/boxes",
-          {
-            name: box.name,
-            lat: box.lat,
-            lng: box.lng,
-            boxType: box.boxType,
-            implanted: true,
-            project: box.project,
-            hierarchyLevel: box.hierarchyLevel,
-            coords: null,
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
-              Authorization: process.env.AUTHORIZATION,
-            },
-            timeout: 2000,
-          }
-        );
+        const res = await axiosInstance.post("boxes", {
+          name: box.name,
+          lat: box.lat,
+          lng: box.lng,
+          boxType: box.boxType,
+          implanted: true,
+          project: box.project,
+          hierarchyLevel: box.hierarchyLevel,
+          coords: null,
+        });
 
         logger.info(`Request succeeded for box: ${box.name || res.data.name}`);
-        response.push(
-          `Request succeeded for box: ${box.name || res.data.name}`
-        );
       } catch (error) {
         logger.error(
-          `Request failed for box: ${box.name || "No box name"}`,
-          error.response.data
-        );
-        response.push(
-          `Request failed for box: ${box.name || "No box name"}, error: ${
-            error.response.data
-          }`
+          `Request failed for box: ${box.name}, Error message: ${error.response.data}`
         );
       }
     });
 
     try {
-      const BATCH_SIZE = 10;
+      await Promise.all(axiosRequests);
 
-      for (let i = 0; i < axiosRequests.length; i += BATCH_SIZE) {
-        const batch = axiosRequests.slice(i, i + BATCH_SIZE);
-        await Promise.all(batch);
-      }
-
-      res
-        .status(200)
-        .json({ message: "Success with return", response: response });
+      res.status(200).json({ message: "Boxes send with success" });
     } catch (error) {
       logger.error(error);
       res.status(500).json({ message: "Something get wrong", error: error });
