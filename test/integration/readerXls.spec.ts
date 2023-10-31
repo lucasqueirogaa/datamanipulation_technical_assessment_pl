a708711689b0e4bdc663aff15f605aca0bd9b89f
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-10-30 22:43:19 -0300

    feat: Change test name and create more verification to boxes

--- test/integration/readerXls.spec.ts
+++ test/integration/readerXls.spec.ts
@@ -1,11 +1,8 @@
-import { MongoClient } from "mongodb";
-import { IBoxes } from "../../src/types/Boxes";
 import supertest from "supertest";
 import { server } from "../../src/api";
 import { connectToDB, disconnectToDB } from "../../src/config/db";
 
 describe("Boxes endpoint", () => {
-  let Boxes: IBoxes[];
   const uri = "mongodb://127.0.0.1:27017/jestdatabase";
   beforeEach(async () => {
     await connectToDB(uri);
@@ -16,8 +13,15 @@ describe("Boxes endpoint", () => {
   });
 
   it("Should create all boxes on DB", async () => {
-    const { status } = await supertest(server).post("/read-xls/boxes");
+    const { status, body } = await supertest(server).post("/read-xls/boxes");
 
     expect(status).toBe(200);
+    expect(body.Boxes.length).toBe(68);
+    expect(body.Boxes[0]).toHaveProperty("name");
+    expect(body.Boxes[0]).toHaveProperty("lat");
+    expect(body.Boxes[0]).toHaveProperty("lng");
+    expect(body.Boxes[0]).toHaveProperty("boxType");
+    expect(body.Boxes[0]).toHaveProperty("project");
+    expect(body.Boxes[0]).toHaveProperty("hierarchyLevel");
   });
 });
