e8f183ee7014967b813dbaebb9d1794d871bc61a
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-10-30 22:47:49 -0300

    feat: Create tests for splitters create on DB

--- test/integration/readerXls.spec.ts
+++ test/integration/readerXls.spec.ts
@@ -2,7 +2,7 @@ import supertest from "supertest";
 import { server } from "../../src/api";
 import { connectToDB, disconnectToDB } from "../../src/config/db";
 
-describe("Boxes endpoint", () => {
+describe("Reader Xls endpoints", () => {
   const uri = "mongodb://127.0.0.1:27017/jestdatabase";
   beforeEach(async () => {
     await connectToDB(uri);
@@ -23,5 +23,21 @@ describe("Boxes endpoint", () => {
     expect(body.Boxes[0]).toHaveProperty("boxType");
     expect(body.Boxes[0]).toHaveProperty("project");
     expect(body.Boxes[0]).toHaveProperty("hierarchyLevel");
+  });
+
+  it("Should create all splitters on DB", async () => {
+    const { status, body } = await supertest(server).post(
+      "/read-xls/splitters"
+    );
+
+    expect(status).toBe(200);
+    expect(body.Splitters.length).toBe(94);
+    expect(body.Splitters[0]).toHaveProperty("implanted");
+    expect(body.Splitters[0]).toHaveProperty("isDrop");
+    expect(body.Splitters[0]).toHaveProperty("parent");
+    expect(body.Splitters[0]).toHaveProperty("project");
+    expect(body.Splitters[0]).toHaveProperty("name");
+    expect(body.Splitters[0]).toHaveProperty("splitterType");
+    expect(body.Splitters[0]).toHaveProperty("ratio");
   });
 });
